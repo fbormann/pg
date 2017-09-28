102126ec48e46e6dffd9654d8fc825da3577f075
albertmourato <ams11@cin.ufpe.br>
2017-09-28 13:34:33 -0300

    right click button added

--- src/Points.java
+++ src/Points.java
@@ -10,21 +10,17 @@ import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 
-public class Points extends JPanel implements MouseListener{
+public class Points extends JPanel implements MouseListener {
 	public static ArrayList<Point> points = new ArrayList();
-	
 
-	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
 
 		Graphics2D g2d = (Graphics2D) g;
 
 		g2d.setColor(Color.red);
 		g2d.setStroke(new BasicStroke(2));
-		
-		
+
 		if (points.size() > 1) {
 			for (int i = 0; i < points.size() - 1; i++) {
 				g2d.drawLine(points.get(i).x, points.get(i).y, points.get(i + 1).x, points.get(i + 1).y);
@@ -47,31 +43,45 @@ public class Points extends JPanel implements MouseListener{
 		Point p1 = new Point();
 		p1.setLocation(x, y);
 		points.add(p1);
-		
+
+		if (e.getButton() == MouseEvent.BUTTON2) {
+			int aux = points.size();
+			if(aux>=0){
+				points.remove(points.size()-1);
+			}else {
+				points.add(p1);
+			}
+		}
+
 		repaint();
 		System.out.println(points.size());
 	}
-	
-	public void mousePressed(MouseEvent e) {}
-	public void mouseReleased(MouseEvent e) {}
-	public void mouseEntered(MouseEvent e) {}
-	public void mouseExited(MouseEvent e) {}
-	
+
+	public void mousePressed(MouseEvent e) {
+	}
+
+	public void mouseReleased(MouseEvent e) {
+	}
+
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	public void mouseExited(MouseEvent e) {
+	}
+
 	public static void main(String[] args) {
 		Points points = new Points();
 		JFrame frame = new JFrame("Points");
 		JMenuBar menuBar = new JMenuBar();
 		JMenu menu = new JMenu("Configurar");
 		JMenuItem menuItem = new JMenuItem("Adicionar Pontos");
-		
-
 
 		menu.add(menuItem);
-		
+
 		menuBar.add(menu);
 		menuBar.setBackground(Color.GRAY);
-		menuBar.setPreferredSize(new Dimension(200, 30));	
-		
+		menuBar.setPreferredSize(new Dimension(200, 30));
+
 		frame.setJMenuBar(menuBar);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(points);
@@ -79,7 +89,7 @@ public class Points extends JPanel implements MouseListener{
 		frame.setLocationRelativeTo(menu);
 		frame.setVisible(true);
 		frame.addMouseListener(points);
-		//frame.addMouseListener();
+		// frame.addMouseListener();
 		// listener(frame);
 	}
 }
